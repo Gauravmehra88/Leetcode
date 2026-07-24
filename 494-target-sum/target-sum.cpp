e918b9f168a829560c915fbd0c30022703e36e90
Gaurav Mehra <151906528+Gauravmehra88@users.noreply.github.com>
2026-07-24 10:15:30 +0530

    Time: 11 ms (56.28%) | Memory: 25.9 MB (21.97%) - LeetSync

--- 494-target-sum/target-sum.cpp
+++ 494-target-sum/target-sum.cpp
@@ -2,10 +2,15 @@ class Solution {
 public:
     int findTargetSumWays(vector<int>& nums, int target) {
         int n=nums.size();
-         return recur(nums , target , 0 , n-1);
+        int total =0;
+        for(int m:nums){
+            total+=m;
+        }
+        vector<vector<int>>dp(n , vector<int>( (total * 2)+1 , -1));
+         return recur(nums , target , 0 , n-1,total, dp);
     }
 
-    int recur(vector<int>& nums , int target , int currsum , int index){
+    int recur(vector<int>& nums , int target , int currsum , int index ,int total , vector<vector<int>> &dp){
         if(index==0){
              int ways = 0;
 
@@ -14,9 +19,12 @@ public:
 
                 return ways;
         }
-        int plus = recur(nums , target , currsum + nums[index] , index-1);
-        int minus = recur(nums , target , currsum - nums[index] , index-1);
+        if(dp[index][currsum +total]!=-1){
+            return dp[index][currsum + total];
+        }
+        int plus = recur(nums , target , currsum + nums[index] , index-1,total ,dp);
+        int minus = recur(nums , target , currsum - nums[index] , index-1,total ,dp);
 
-        return plus + minus;
+        return  dp[index][currsum + total] = plus + minus;
     }
 };
