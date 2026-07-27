51183b2bbafed131e4b219ae5fc9de74a8907bf6
Gaurav Mehra <151906528+Gauravmehra88@users.noreply.github.com>
2026-07-27 12:40:01 +0530

    Time: 27 ms (56.89%) | Memory: 27.6 MB (31.56%) - LeetSync

--- 1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -1,28 +1,47 @@
 class Solution {
 public:
     int longestCommonSubsequence(string text1, string text2) {
-        int n=text1.length();
-        int m = text2.length();
-        vector<vector<int>>dp(n , vector<int>(m+1 , -1));
-        return recur(text1 , text2 , n-1 , m-1 ,dp);
-    }
-    int recur(string &text1 , string &text2 , int idx1 , int idx2, vector<vector<int>>& dp){
+        int n= text1.size() ;
+        int m = text2.size();
 
-        if(idx1<0 || idx2<0) return 0;
-
-        if(dp[idx1][idx2]!=-1){
-            return dp[idx1][idx2];
-        }
-         if(text1[idx1]==text2[idx2]){
-            return dp[idx1][idx2]=1 + recur(text1 , text2 , idx1-1 , idx2-1 ,dp);
-        }
-        else{
+        vector<vector<int>>dp( n+1 , vector<int>(m+1));
         
-        int t1= 0 + recur(text1 , text2 , idx1-1 , idx2,dp);
-        int t2=0 + recur(text1 , text2 , idx1 , idx2-1,dp);
-        return dp[idx1][idx2] = max(t1,t2);
+        for(int i=0;i<=n;i++){
+            dp[i][0]=0;
         }
 
-        return dp[idx1][idx2] ;
+         for(int j=0;j<=m;j++){
+            dp[0][j]=0;
+        }
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(text1[i-1] == text2[j-1]){
+                    dp[i][j] = 1 + dp[i-1][j-1];
+                }
+                else{
+                    dp[i][j] = max(dp[i-1][j] , dp[i][j-1]);
+                }
+            }
+        }
+
+        // string lcs ="";
+        // for(int i=1;i<n;i++){
+        //     for(int j=1;j<n;j++){
+        //         if(text1[i-1]==text2[j-1]){
+        //             lcs.push_back(text1[i-1]);
+        //             i--;
+        //             j--;
+        //         }
+        //         else if(text1[i-1] >= text2[j-1]){
+        //              i--;
+        //         }
+        //         else{ 
+        //             j--;
+        //         }
+        //     }
+        // }
+        // reverse(lcs.begin(), lcs.end());
+        // return lcs;
+        return dp[n][m];
     }
 };
