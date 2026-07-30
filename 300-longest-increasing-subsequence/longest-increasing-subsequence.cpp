723c9d27fa458c5a6d3338d76c9d767dc7da6532
Gaurav Mehra <151906528+Gauravmehra88@users.noreply.github.com>
2026-07-30 10:48:06 +0530

    Time: 51 ms (70.16%) | Memory: 14.4 MB (61.89%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -2,22 +2,18 @@ class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
         int n=nums.size();
-        vector<vector<int>>dp(n ,vector<int>(n+1,-1));
-        return recur(nums , 0 , -1,dp);
-    }
-    int recur( vector<int>& nums , int idx, int previdx , vector<vector<int>> &dp){
-         if(idx==nums.size()){
-            return 0;
-         }
-         if(dp[idx][previdx + 1] != -1){
-            return dp[idx][previdx+1];
-         }
-         int pick=0;
-         if( previdx == -1  || nums[idx]>nums[previdx]){
-            pick = 1 + recur(nums, idx+1 , idx ,dp);
-         }
-         int nopick = recur(nums , idx+1 , previdx , dp);
-
-         return dp[idx][previdx+1] = max(pick,nopick);
+        vector<int>lis(n,1);
+        int maxLen=1;
+        for(int i=1;i<n;i++){
+            for(int p=0;p<i;p++){
+                if(nums[i]>nums[p]){
+                        lis[i]=max(lis[i], 1 + lis[p]);
+                }
+            }
+            if(lis[i]>maxLen){
+                maxLen = lis[i];
+            }
+        }
+        return maxLen;
     }
 };
