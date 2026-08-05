f0af81e02e9977ee3624237e2b30224488f81da4
Gaurav Mehra <151906528+Gauravmehra88@users.noreply.github.com>
2026-08-05 19:51:09 +0530

    Time: 0 ms (100.00%) | Memory: 10 MB (39.34%) - LeetSync

--- 486-predict-the-winner/predict-the-winner.cpp
+++ 486-predict-the-winner/predict-the-winner.cpp
@@ -3,7 +3,8 @@ public:
     bool predictTheWinner(vector<int>& nums) {
         int n = nums.size();
          int  total = accumulate(nums.begin() , nums.end() , 0);
-        int p1 = recur(nums , 0 , n-1);
+         vector<vector<int>>dp(n , vector<int>(n , -1));
+        int p1 = recur(nums , 0 , n-1 , dp);
         int p2 = total - p1;
         if(p1>=p2){
             return true;
@@ -12,14 +13,17 @@ public:
         
     }
 
-     int recur(vector<int> &nums , int i , int j  ){
+     int recur(vector<int> &nums , int i , int j ,vector<vector<int>>&dp ){
         if(i>j) return 0;
         if(i==j){
             return nums[i];
         }
-        int pick_i = nums[i] + min( recur(nums , i+2 , j) , recur(nums , i+1 , j-1));
-        int pick_j = nums[j] + min( recur(nums , i , j-2) , recur(nums , i+1 , j-1));
+        if(dp[i][j]!=-1){
+            return dp[i][j];
+        }
+        int pick_i = nums[i] + min( recur(nums , i+2 , j,dp) , recur(nums , i+1 , j-1,dp));
+        int pick_j = nums[j] + min( recur(nums , i , j-2,dp) , recur(nums , i+1 , j-1,dp));
 
-        return max(pick_i , pick_j);
+        return dp[i][j] = max(pick_i , pick_j);
      }
 };
